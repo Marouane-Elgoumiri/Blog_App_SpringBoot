0b417275098ab3ddd411afa89fcc8c5b1b750571
Marouane-Elgoumiri <mar.elgoumiri@hotmail.com>
2024-04-17 11:54:15 +0100

    first commit

---
+++ src/main/java/com/example/blog_app_springboot/BlogAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_app_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppSpringBootApplication.class, args);
+    }
+
+}
